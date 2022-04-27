8c5b8a08477d73e8fae1dc01e904725a77688fda
ShootGan <nawelpalepka@gmail.com>
2022-04-27 20:31:03 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+setuptools.setup(
+    name="Code generator",
+    author="Pawel Nalepka",
+    author_email="nawelpalepka@gmailcom",
+    keywords=["open source"],
+    description="yep",
+    license="MIT License",
+    url="",
+    version="0.1.0",
+    download_url="",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    classifiers=[
+        "License :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
